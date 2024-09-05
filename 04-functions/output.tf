ffdc5679099bc13d25bc8f3bafe10dd1acb4e843
Sridevi-kr <sridevi.kandhalu@gmail.com>
2024-09-05 15:05:49 -0500

    usage of built in functions

---
+++ 04-functions/output.tf
@@ -0,0 +1,14 @@
+variable "fruits" {}
+
+# Using lookup funciton, we can return a default value of the key is not defined
+output "apple" {
+  value = " Apple color is ${lookup(var.fruits["apple"], "color", "green")} and  price is  ${lookup(var.fruits["apple"], "price", "100")}"
+}
+
+output "grapes" {
+  value = " Grapes color is  ${lookup(var.fruits["grapes"], "color", "green")} and price is  ${lookup(var.fruits["grapes"], "rate", "50")}"
+}
+
+# other way is 
+output "apple" {
+    value = "Apple color is ${var.fruits["apple"]["color"]} and ${var.fruits["apple"]["metric"]} price is 4{var.fruits["apple"]["price"]}"
---
+++ 04-functions/sample.auto.tfvars
@@ -0,0 +1,16 @@
+#    fruit = orange
+
+# This is another way of supplying input and this helps in avoiding the need of declaring multiple variables
+fruits = {
+  apple = {
+    color = "red"
+    price  = 100
+    metric = "kg"
+  }
+
+  grapes = {
+    color  = "black"
+    price  = 120
+    metric = "kg"
+  }
+}
