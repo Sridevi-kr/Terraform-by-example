bb3617eaf4a6e5cdd8c2f8742c4e8a512ffd395a
Sridevi-kr <sridevi.kandhalu@gmail.com>
2024-09-06 21:50:43 -0500

    second version with code optimization

--- expense-v2/ec2.tf
+++ expense-v2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "main" {
   count                  = length(var.components)
   ami                    = "ami-0fcc78c828f981df2"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-08c9eb09595f5de07"]
+  vpc_security_group_ids = ["sg-0a65d86379df4012d"]
 
   tags = {
     Name = var.components[count.index]
