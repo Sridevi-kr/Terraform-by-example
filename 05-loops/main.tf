d1a25f949967dfcf7313006e24ef02da32a6536b
Sridevi-kr <sridevi.kandhalu@gmail.com>
2024-09-06 21:11:13 -0500

    loops with length function

--- 05-loops/main.tf
+++ 05-loops/main.tf
@@ -1,4 +1,14 @@
-resource "null_resource" "main"{
- count = 3
+# Basic
+resource "null_resource" "main" {
+  count = 3 # count ia a meta argument that goes by list and each and every iteration is defined by a index number
 }
 
+# Dynamic
+
+resource "null_resource" "fruits" {
+  count = length(var.fruits)
+}
+
+variable "fruits" {
+  default = ["grapes", "orange", "pear"]
+}
